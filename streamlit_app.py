c9f9c36c3c06b2d85502d3bebb53fb2208b60caf
GOWTHAM HARI S <gowthamsengodan7@gmail.com>
2026-03-18 09:54:52 +0530

    Update streamlit_app.py
    Changed the title for testing streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # App Title & Description
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title(":cup_with_straw: My Parents' healthy Dinner :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 # New way to connect for SniS
 cnx = st.connection("snowflake")
